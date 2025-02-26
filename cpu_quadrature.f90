a9fe7bdf4f3cade32d4bccae6eb8d61176d522a3
RichardThompson1 <richard.thompson111f3@gmail.com>
2025-02-26 12:32:34 -0500

    updated cpu quadrature for parallel execution

--- cpu_quadrature.f90
+++ cpu_quadrature.f90
@@ -3,7 +3,7 @@ module quadrature_module_cpu
   
   contains
 
-  subroutine quadrature_cpu(x_array, y_array, t_array, result_array, hx, hy, n, m)
+  subroutine quadrature_cpu_serial(x_array, y_array, t_array, result_array, hx, hy, n, m)
     implicit none
     
     integer, parameter :: dp = kind(1.0d0)
@@ -33,14 +33,55 @@ module quadrature_module_cpu
           end if
           
           f = sin(n * pi * x_array(i)) * sin(m * pi * y_array(j))
-          if (f == 0) then
-            print *, 'No contribution at: (',i,j,k,') (x,y,t)'
-          end if
+          ! if (f == 0) then
+          !   print *, 'No contribution at: (',i,j,k,') (x,y,t)'
+          ! end if
           result_array(k) = result_array(k) + (w_ij * f) * (hx * hy / 4.0)
         end do
       end do
     end do
 
+  end subroutine
+
+  subroutine quadrature_cpu_parallel(x_array, y_array, t_array, result_array, hx, hy, n, m)
+    implicit none
+    
+    integer, parameter :: dp = kind(1.0d0)
+    real, intent(in) :: x_array(:)
+    real, intent(in) :: y_array(:)
+    real, intent(in) :: t_array(:)
+    integer, intent(in) :: n, m
+    real(kind=dp), intent(in) :: hx, hy
+    real(kind=dp), intent(out) :: result_array(:)
+
+    integer :: i, j, k
+    real :: f
+    real :: pi                       
+    real :: w_ij
+    real(kind=dp) :: local_sum
+
+    pi = 4*atan(1.)
+
+    !$OMP PARALLEL DO SCHEDULE(DYNAMIC) PRIVATE(i,j,f,w_ij,local_sum)
+    do k = 1, size(t_array)
+      local_sum = 0.0
+      do i = 1, size(x_array)
+        do j = 1, size(y_array)
+          if ((i == 1 .or. i == size(x_array)) .and. (j == 1 .or. j == size(y_array))) then
+            w_ij = 1.0
+          else if ((i == 1 .or. i == size(x_array)) .or. (j == 1 .or. j == size(y_array))) then
+            w_ij = 2.0
+          else
+            w_ij = 4.0
+          end if
+          
+          f = sin(n * pi * x_array(i)) * sin(m * pi * y_array(j))
+          local_sum = local_sum + (w_ij * f) * (hx * hy / 4.0)
+        end do
+      end do
+      result_array(k) = local_sum
+    end do
+    !$OMP END PARALLEL DO
   end subroutine
 
 end module quadrature_module_cpu
